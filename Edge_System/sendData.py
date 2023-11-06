dc0f77017a249ca21f413d60beaa6db367553683
star0504 <sdkyu12345@gmail.com>
2023-11-07 00:46:30 +0900

    .

--- Edge_System/sendData.py
+++ Edge_System/sendData.py
@@ -2,62 +2,65 @@ import os
 import cv2
 import pathlib
 from pathlib import Path
-import requests 
+import requests
 from datetime import datetime
 from utils.general import increment_path
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
-project=ROOT / '.runs/detect/exp',  # save results to project/name
-exist_ok=False,  # existing project/name ok, do not increment
-save_txt=False,  # save results to *.txt
-name='exp',  # save results to project/name
+project = ROOT / '.runs/detect/exp',  # save results to project/name
+exist_ok = False,  # existing project/name ok, do not increment
+save_txt = False,  # save results to *.txt
+name = 'exp',  # save results to project/name
 
-counter = 1
 
 class SendData:
-    HOST = "http://127.0.0.1:8000"
+    HOST = "http://sdkyu12345.pythonanywhere.com"
     username = "admin"
     password = "my0504"
     token = ''
     title = ''
     text = ''
-    
+
     def __init__(self):
         res = requests.post(self.HOST + '/api-token-auth/', {
             'username': self.username,
-            'password' : self.password
+            'password': self.password
         })
-        
+
         res.raise_for_status()
-        
+
         self.token = res.json()['token']
 
     def send(self, request, saveName):
         now = datetime.now()
         now.isoformat()
-        save_dir = increment_path('runs/detect/exp', exist_ok=exist_ok)  # increment run
-        
-        self.title = counter + request["title"]
+        save_dir = increment_path(
+            'runs/detect/exp', exist_ok=exist_ok)  # increment run
+
+        self.title = "위반 차량 발견"
         self.text = request["text"]
         image = request["image"]
-        
+
         today = datetime.now()
-        save_path = (os.getcwd() / save_dir / 'detected' /str(today.year) / str(today.month) / str(today.day))
+        save_path = (os.getcwd() / save_dir / 'detected' /
+                     str(today.year) / str(today.month) / str(today.day))
         full_path = save_path / saveName
-        
-        dst = cv2.resize(image, dsize=(320, 240), interpolation = cv2.INTER_AREA)
+
+        dst = cv2.resize(image, dsize=(320, 240), interpolation=cv2.INTER_AREA)
         # cv2.imwrite(full_path, dst)
-        
-        headers = {'Authorization' : 'JWT ' + self.token, 'Accept' : 'application/json'}
-        
+
+        headers = {'Authorization': 'JWT ' +
+                   self.token, 'Accept': 'application/json'}
+
         data = {
-            'author' : 1,
-            'title' : self.title,
-            'text' : self.text,
-            'created_date' : now,
-            'published_date' : now
+            'author': 1,
+            'title': self.title,
+            'text': self.text,
+            'created_date': now,
+            'published_date': now
         }
-        file = {'image' : open(full_path, 'rb')}
-        res = requests.post(self.HOST + '/api_root/Post/', data=data, files=file, headers=headers)
+        file = {'image': open(full_path, 'rb')}
+        res = requests.post(self.HOST + '/api_root/Post/',
+                            data=data, files=file, headers=headers)
         print(res)
